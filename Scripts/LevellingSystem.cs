e1172a9e50b8dccaf10d3e27c76d931046c3c3c6
Armanix <a.shaikh@student.saedubai.com>
2022-06-17 00:41:51 +0400

    Ahmed Tolba <3

---
+++ Scripts/LevellingSystem.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class holds all the logic for our levelling system, so that includes logic to handle levelling up, gaining xp and detecting when we should level up!.
+/// </summary>
+public class LevellingSystem : MonoBehaviour
+{
+    public int currentLevel; // Our current level.
+
+    public int currentXp; // The current amount of xp we have accumulated.
+
+    public int currentXPThreshold = 10; // The amount of xp required to level up.
+
+    private void Start()
+    {
+
+        
+      
+
+
+        // set our current level to one.
+        currentLevel = 1;
+
+        // set our current XP to zero
+        currentXp = 0;
+
+        // set our current XP Threshold to be our level multiplied by our 100.
+        currentXPThreshold = currentLevel * 100;
+
+        // Debug out our starting values of our level, xp and current xp threshold
+        Debug.Log(currentLevel);
+        Debug.Log(currentXp);
+        Debug.Log(currentXPThreshold);
+
+        // Increase the current XP by one hundred.
+        currentXp += 100;
+
+        // Debug out our current XP.
+        Debug.Log(currentXp);
+
+        // check if our current XP is more than our threshold.
+        if (currentXp > currentXPThreshold)
+
+        // if it is, then let's increase out level by one.
+        {
+            currentLevel = (currentLevel + 1);
+        }
+
+        // let's also increase recalculate our current xp threshold as we've levelled up.
+        currentXPThreshold = currentLevel * 100;
+        // Debug out our new level value, as well as our current XP and our next Threshold we need to hit.
+        Debug.Log(currentLevel);
+        Debug.Log(currentXp);
+        Debug.Log(currentXPThreshold);
+
+    }
+}
---
+++ Scripts/StatsSystem.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class holds all the logic for our stats system, so that includes logic to handle generating starting physical stats
+/// calculating our dancing stats based on physical stats and our stat multipliers.
+/// </summary>
+public class StatsSystem : MonoBehaviour
+{
+    /// Our physical stats that determine our dancing stats.
+    public int agility;
+    public int intelligence;
+    public int strength;
+
+    // Our variables used to determine our fighting power.
+    public int style;
+    public int luck;
+    public int rhythm;
+
+    private void Start()
+    {
+        // set out agility, strength and intelligence to a random number between zero and ten.
+        // BONUS! let's try taking our stats away from a pool of stats, i.e. 20 total, distributing this amoungst all the stats.
+        int totalPool = 20;
+        int remainingPool = totalPool;
+        strength = Random.Range(0, 11);
+        remainingPool -= strength;
+     
+        agility = Random.Range(0, 11);
+        remainingPool -= agility;
+        agility = totalPool - (remainingPool + strength);
+       
+        intelligence = remainingPool;
+
+        // Debug out your current physical stat values (strength, agility, intelligence).
+        Debug.Log(strength);
+        Debug.Log(agility);
+        Debug.Log(intelligence);
+
+        // let's create some float temporary variables to hold our multiplier values.
+
+        // create an agility multiplier should be set to 0.5
+        float agilityMulti = 0.5f;
+        // create a strength multiplier should be set to 1
+        float strengthMulti = 1f;
+        // create an intelligence multiplier should be set to 2.
+        float inteligenceMulti = 2f;
+        // Debug out our current multiplier values.
+        Debug.Log(agilityMulti);
+        Debug.Log(strengthMulti);
+        Debug.Log(inteligenceMulti);
+        // now that we have some stats and our multiplier values let's calculate our style, luck and ryhtmn based on these values.
+
+        // style should be based off our strength and be converted at a rate of 1 : 1.
+        style = 1;
+        float styleF = (float)style; // I changed it to float so i can multiply it by the multiplier.
+        styleF *= strengthMulti;
+        // luck should be based off our intelligence and be converted at a rate of 1 : 1.5f
+        luck = 1;
+        float luckF = (float)luck;
+        luckF *= inteligenceMulti;
+        // rhythm should be based off our agility and be converted at a rate of 1 : 0.5 .
+        rhythm = 1;
+        float rhythmF = (float)rhythm;
+        rhythmF *= agilityMulti;
+
+        // Debug out our current dancing stat values (style, luck, rhythm)
+        Debug.Log(styleF);
+        Debug.Log(luckF);
+        Debug.Log(rhythmF);
+
+        // now let's imagine that our level has increased; and we've been granted 10 new stat points.
+        // let's distribute those stats amoungst our strength and agility and intelligence.
+        int additionalPoints = 10;
+
+        // Debug out our new physical stat values
+
+        // let's recalculate our style, luck and rhytmn as our initial stats have changed.
+
+        // Debug out our new dancing stat values
+    }
+}
